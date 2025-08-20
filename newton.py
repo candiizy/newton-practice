6f8fac52383cb18d349228619c598486de09e471
candiizy <candyz@berkeley.edu>
2025-08-20 10:41:44 -0700

    Add basic implementation of Newton's method.

---
+++ newton.py
@@ -0,0 +1,7 @@
+def function(x, f,epsilon=1e-6):
+    f_prime = (f(x + epsilon)-f(x))/epsilon
+    f_second_derivative = (f_prime(x + epsilon)-f_prime(x))/epsilon ???????
+    return slope = f_prime/f_second_derivative
+for t in range(1, 100):
+    return function
+if f_prime/f_second_derivative < epsilon :
